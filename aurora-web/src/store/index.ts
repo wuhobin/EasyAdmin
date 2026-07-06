4025dc0d456102c6f516f331d0590fe69bdeabe4
wuhongbin <wuhongbin@nl-ds.cn>
2026-07-06 17:21:10 +0800

    Revert "feat(pinia): 注册 pinia-plugin-persistedstate 插件"
    This reverts commit 05bff1218ce5a04ea915ae2e093fef2d3e264b73.

--- aurora-web/src/store/index.ts
+++ aurora-web/src/store/index.ts
@@ -1,9 +1,7 @@
 import type { App } from "vue";
 import { createPinia } from "pinia";
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const store = createPinia();
-store.use(piniaPluginPersistedstate)
 
 // 全局注册 store
 export function setupStore(app: App<Element>) {
